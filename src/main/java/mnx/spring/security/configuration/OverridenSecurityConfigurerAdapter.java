f991240bcb2a2f54a6dfdeb322a44ce3a69b8d8f
Mnx-one <Monax@csgo1>
2022-03-09 00:25:52 +0530

    initial commit

---
+++ src/main/java/mnx/spring/security/configuration/OverridenSecurityConfigurerAdapter.java
@@ -0,0 +1,23 @@
+package mnx.spring.security.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class OverridenSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/exclude/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
